c81a42afec10f52eca1fc880467f38e7954bc30d
Bernard Suen <bsysin@gmail.com>
2019-02-12 01:12:56 +0800

    Update to flask_app.py
    Update to flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, json, redirect, session
 from flask import Markup
-import requests,  MySQLdb
+import requests
 
 app = Flask(__name__)
 app.config["DEBUG"] = False
