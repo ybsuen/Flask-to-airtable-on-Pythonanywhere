c6f49f1bea8c83129d4db2af04b55586deca8f81
Bernard Suen <bsysin@gmail.com>
2019-02-18 14:33:24 +0800

    Flask Airtable Integration Update
    Add C3 Chart to the App

--- flask_app.py
+++ flask_app.py
@@ -60,6 +60,30 @@ def table():
          dataset.append(dict)
     return render_template('table.html', entries=dataset)
 
+@app.route("/chart")
+def chart():
+    headers = {
+        'Authorization': 'Bearer keyTcsTzckqyBTlk8',
+    }
+
+    params = (
+        ('view', 'Grid view'),
+    )
+
+    r = requests.get('https://api.airtable.com/v0/appM38HXlEVhxmnqx/Stage?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc', headers=headers, params=params)
+    dict1 = r.json()
+    dict2 = {}
+    dataset = []
+    name_list = []
+    total_entries_list = []
+    for i in dict1['records']:
+         dict2 = i['fields']
+         dataset.append(dict2)
+    for item in dataset:
+        name_list.append(item.get('Name'))
+        total_entries_list.append(item.get('Total_Entries'))
+    return render_template('flask_chart.html', entries = zip(name_list, total_entries_list))    
+
 
 @app.route("/login")
 def login():
